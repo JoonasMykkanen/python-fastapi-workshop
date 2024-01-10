8e039cdc439b7c8798efb4ee4222c9a2f2c5e5dc
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2024-01-10 14:39:37 +0200

    pushing second endpoint, optimally would split code for different routes etc but since small application, a bit overkill.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,95 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    main.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/01/10 13:41:36 by jmykkane          #+#    #+#              #
+#    Updated: 2024/01/10 14:37:32 by jmykkane         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Query
+from pprint import pprint
+from fastapi import Path
+import pathlib
+import json
 
-app = FastAPI(title="Restaurant API")
+class Coordinates(BaseModel):
+    lat: float
+    lon: float
 
+class Location(BaseModel):
+    city: str
+    coordinates: Coordinates
 
-@app.get("/")
+class Restaurant(BaseModel):
+    name: str
+    id: str
+    description: str
+    location: Location
+
+app = FastAPI(title='Restaurant API by jmykkane')
+
+@app.get('/')
 def hello_world():
-    return {"Hello": "World"}
+    return 'Nothing to see here'
+
+@app.get('/restaurants')
+def get_restaurants() -> list[Restaurant]:
+    file_path = pathlib.Path(__file__).parent / 'restaurants.json'
+    with open(file_path) as file:
+        data = json.load(file)
+
+    if not data:
+        return {'message': 'Error with database'}, 500
+
+    restaurants = []
+    for restaurant in data['restaurants']:
+        name = restaurant['name']
+        id  = restaurant['id']
+        description = restaurant['description']
+        lon, lat = restaurant['location']
+        city = restaurant['city']
+        
+        restaurants.append(Restaurant(
+            name=name,
+            id=id,
+            description=description,
+            location=Location(city=city, coordinates=(Coordinates(lat=lat, lon=lon))),
+        ))
+
+    return restaurants
+
+@app.get('/restaurants/{id}')
+def get_one_restaurant(id):
+    file_path = pathlib.Path(__file__).parent / 'restaurants.json'
+    with open(file_path) as file:
+        data = json.load(file)
+
+    if not data:
+        return {'message': 'Error with database'}, 500
+
+    try:
+        restaurants = []
+        for restaurant in data['restaurants']:
+            if restaurant['id'] == id:
+                lon, lat = restaurant['location']
+                response = Restaurant(
+                    name=restaurant['name'],
+                    id = restaurant['id'],
+                    description = restaurant['description'],
+                    location=Location(city=restaurant['city'], coordinates=Coordinates(lat=lat, lon=lon))
+                )
+                if response:
+                    return response
+                else:
+                    return {'Message': 'not found'}, 404
+
+    except Exception as e:
+        print(f'Error at get:restaurants/id: {e}')
+        return {'Message': f'internal server error: {e}'}, 500
+    
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,21 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    __init__.py                                        :+:      :+:    :+:    #
+#    test_app.py                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/01/10 13:41:39 by jmykkane          #+#    #+#              #
-#    Updated: 2024/01/10 13:41:39 by jmykkane         ###   ########.fr        #
+#    Created: 2024/01/10 14:06:47 by jmykkane          #+#    #+#              #
+#    Updated: 2024/01/10 14:09:39 by jmykkane         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+from fastapi.testclient import TestClient
+from app.main import app
+
+def test_get_restaurants():
+	with TestClient(app) as client:
+		response = client.get("/restaurants")
+
+		assert response.status_code == 200
+		assert len(response.json()) == 20
